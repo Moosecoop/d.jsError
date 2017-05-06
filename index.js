9d1744841cd3a0516a659447fd2d7bca4400f522
moosecoop <cooper@makhijani.us>
2017-05-06 10:52:12 -0700

    colored err box

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 'use strict';
+const Discord = require('discord.js');
 
 /**
  * Adds commas to a number
- * @param {number} number
+ * @param {string} err
  * @param {string} locale
  * @return {string}
  */
-module.exports = function(err, num, contact) {
-    var errorToSend = `ERR ${num}\n${err}\nPlease contact ${contact} and show them this message!`;
+module.exports = function (err, num, contact) {
+    var errorToSend = `ERR \`\`\`CSS\n${num}\n${err}\`\`\`\nPlease contact ${contact} and show them this message!`;
     return errorToSend;
 };
